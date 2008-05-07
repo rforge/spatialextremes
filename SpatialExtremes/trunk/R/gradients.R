e0895e41bcf80e8e363b22312c914a537100ae40
ribatet <ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-05-07 15:42:35 +0000

    Add the gradient computation for the schlather's model. Renaming function gradxxx to .gradxxxx
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SpatialExtremes/trunk/R/gradients.R
+++ SpatialExtremes/trunk/R/gradients.R
@@ -1,5 +1,6 @@
-smithgrad <- function(par, data, distVec, loc.dsgn.mat, scale.dsgn.mat,
-                      shape.dsgn.mat, fit.marge, std.err.type = "score"){
+.smithgrad <- function(par, data, distVec, loc.dsgn.mat,
+                       scale.dsgn.mat, shape.dsgn.mat, fit.marge,
+                       std.err.type = "score"){
 
   ##data is a matrix with each column corresponds to one location
   ##distVec is the a matrix giving the "distance vector" for each pair
@@ -32,15 +33,15 @@ smithgrad <- function(par, data, distVec, loc.dsgn.mat, scale.dsgn.mat,
     else
       shape.names <- paste("shapeCoeff", 1:n.shapecoeff, sep="")
   
-    param <- c("icov11", "icov12", "icov22", loc.names, scale.names, shape.names)
+    param <- c("cov11", "cov12", "cov22", loc.names, scale.names, shape.names)
 
     loc.idx <- which(substr(names(par), 1, 3) == "loc")
     scale.idx <- which(substr(names(par), 1, 5) == "scale")
     shape.idx <- which(substr(names(par), 1, 5) == "shape")
 
-    icov11 <- par["icov11"]
-    icov12 <- par["icov12"]
-    icov22 <- par["icov22"]
+    cov11 <- par["cov11"]
+    cov12 <- par["cov12"]
+    cov22 <- par["cov22"]
     loc.param <- par[loc.idx]
     scale.param <- par[scale.idx]
     shape.param <- par[shape.idx]
@@ -50,9 +51,9 @@ smithgrad <- function(par, data, distVec, loc.dsgn.mat, scale.dsgn.mat,
     n.loccoeff <- 1
     n.scalecoeff <- 1
     n.shapecoeff <- 1
-    icov11 <- par["icov11"]
-    icov12 <- par["icov12"]
-    icov22 <- par["icov22"]
+    cov11 <- par["cov11"]
+    cov12 <- par["cov12"]
+    cov22 <- par["cov22"]
     loc.param <- 1
     scale.param <- 1
     shape.param <- 1
@@ -62,10 +63,95 @@ smithgrad <- function(par, data, distVec, loc.dsgn.mat, scale.dsgn.mat,
              as.integer(n.obs), as.double(loc.dsgn.mat), as.integer(n.loccoeff),
              as.double(scale.dsgn.mat), as.integer(n.scalecoeff), as.double(shape.dsgn.mat),
              as.integer(n.shapecoeff), as.double(loc.param), as.double(scale.param),
-             as.double(shape.param), as.double(icov11), as.double(icov12),
-             as.double(icov22), fit.marge, grad = double(n.obs * 3), PACKAGE = "SpatialExtremes")$grad
+             as.double(shape.param), as.double(cov11), as.double(cov12),
+             as.double(cov22), fit.marge, grad = double(n.obs * 3), PACKAGE = "SpatialExtremes")$grad
 
   grad <- matrix(grad, nrow = n.obs, ncol = 3)
+
+  if (std.err.type == "score")
+    jacobian <- var(grad) * n.obs
+
+  if (std.err.type == "grad"){
+    jacobian <- 0
+    for (i in 1:n.obs){
+      grad.vec <- matrix(grad[i,], ncol = 1)
+      jacobian <- jacobian + grad.vec %*% t(grad.vec)
+    }
+  }
+
+  return(jacobian)
+}
+
+.schlathergrad <- function(par, data, dist, cov.mod, loc.dsgn.mat,
+                           scale.dsgn.mat, shape.dsgn.mat, fit.marge,
+                           std.err.type = "score"){
+
+  ##data is a matrix with each column corresponds to one location
+  ##distVec is the a matrix giving the "distance vector" for each pair
+  ##(1 row = 1 station)
+  n.site <- ncol(data)
+  n.obs <- nrow(data)
+  n.pairs <- n.site * (n.site - 1) / 2
+    
+  if (fit.marge){
+
+     n.loccoeff <- ncol(loc.dsgn.mat)
+     n.scalecoeff <- ncol(scale.dsgn.mat)
+     n.shapecoeff <- ncol(shape.dsgn.mat)
+
+    if (n.loccoeff == 1)
+      loc.names <- "locCoeff"
+    
+    else
+      loc.names <- paste("locCoeff", 1:n.loccoeff, sep="")
+    
+    if (n.scalecoeff == 1)
+      scale.names <- "scaleCoeff"
+    
+    else
+      scale.names <- paste("scaleCoeff", 1:n.scalecoeff, sep="")
+    
+    if (n.shapecoeff == 1)
+      shape.names <- "shapeCoeff"
+    
+    else
+      shape.names <- paste("shapeCoeff", 1:n.shapecoeff, sep="")
+  
+    param <- c("scale", "smooth", loc.names, scale.names, shape.names)
+
+    loc.idx <- which(substr(names(par), 1, 3) == "loc")
+    scale.idx <- which(substr(names(par), 1, 5) == "scale")
+    shape.idx <- which(substr(names(par), 1, 5) == "shape")
+
+    scale <- par["scale"]
+    smooth <- par["smooth"]
+    loc.param <- par[loc.idx]
+    scale.param <- par[scale.idx]
+    shape.param <- par[shape.idx]
+  }
+
+  else {
+    n.loccoeff <- 1
+    n.scalecoeff <- 1
+    n.shapecoeff <- 1
+    scale <- par["scale"]
+    smooth <- par["smooth"]
+    loc.param <- 1
+    scale.param <- 1
+    shape.param <- 1
+  }
+  
+  grad <- .C("schlathergrad", as.integer(cov.mod), as.double(data),
+             as.double(dist), as.integer(n.site),
+             as.integer(n.obs), as.double(loc.dsgn.mat),
+             as.integer(n.loccoeff), as.double(scale.dsgn.mat),
+             as.integer(n.scalecoeff), as.double(shape.dsgn.mat),
+             as.integer(n.shapecoeff), as.double(loc.param),
+             as.double(scale.param), as.double(shape.param),
+             as.double(scale), as.double(smooth), fit.marge, grad =
+             double(n.obs * 2), PACKAGE = "SpatialExtremes")$grad
+
+  grad <- matrix(grad, nrow = n.obs, ncol = 2)
 
   if (std.err.type == "score")
     jacobian <- var(grad) * n.obs
