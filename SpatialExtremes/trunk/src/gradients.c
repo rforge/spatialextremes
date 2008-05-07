ee73b439bb63f0d61c4ca1b0d7b2e74458061eff
ribatet <ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-05-07 15:49:06 +0000

    New function to compute the gradient for the Schlather's model
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SpatialExtremes/trunk/src/gradients.c
+++ SpatialExtremes/trunk/src/gradients.c
@@ -266,5 +266,140 @@ void smithgrad(double *data, double *distVec, int *nSite,
    
    return;
 }
+
+
+void schlathergrad(int *covmod, double *data, double *dist, int *nSite,
+		   int *nObs, double *locdsgnmat, int *nloccoeff,
+		   double *scaledsgnmat, int *nscalecoeff, double *shapedsgnmat,
+		   int *nshapecoeff, double *loccoeff, double *scalecoeff,
+		   double *shapecoeff, double *scale, double *smooth,
+		   int *fitmarge, double *grad){
+
+  //This is the Smith model. It computes the gradient of the pairwise log-likelihood
+  
+  struct toFrech toFrechObj;
+  struct covComp covCompObj;
+  struct toParam gevParam;
+  const int nPairs = *nSite * (*nSite - 1) / 2;
+  int i, j, k, currentPair = -1;
+  double c1, dA, B, dB, C, dC, D, dD,
+    *rho, *locs, *scales, *shapes,
+    jacCommon;
+  //c1 is a useful quantity
+  //A, B, C, D are part of the log-bivariate density
+  //dB, dC and dD are their derivatives with respect to the
+  //covariance function
+  //jacCommon is the common part of all the jacobians - i.e. the common
+  //part when deriving with respect to cov11, cov12 or cov22
+  
+  rho = (double *)R_alloc(nPairs, sizeof(double));
+  locs = (double *)R_alloc(*nSite, sizeof(double));
+  scales = (double *)R_alloc(*nSite, sizeof(double));
+  shapes = (double *)R_alloc(*nSite, sizeof(double));
+
+  //Stage 0: Compute the covariance at each location
+  switch (*covmod){
+  case 1:
+    covCompObj = whittleMatern(dist, nPairs, *scale, *smooth);
+    break;
+  case 2:
+    covCompObj = cauchy(dist, nPairs, *scale, *smooth);
+    break;
+  case 3:
+    covCompObj = powerExp(dist, nPairs, *scale, *smooth);
+    break;
+  }
+  
+  rho = covCompObj.vec;
+
+  //Compute the GEV parameters using the design matrix
+  if (*fitmarge){
+    
+    gevParam = dsgnmat2Param(locdsgnmat, scaledsgnmat, shapedsgnmat,
+			     loccoeff, scalecoeff, shapecoeff, *nSite,
+			     *nloccoeff, *nscalecoeff, *nshapecoeff);
+    
+    locs = gevParam.locs;
+    scales = gevParam.scales;
+    shapes = gevParam.shapes;
+
+    //Stage 1: Transformation to unit Frechet
+    toFrechObj = gev2frech(data, *nObs, *nSite, locs, scales, shapes);
+    
+    data = toFrechObj.frech;
+  }
+  
+  //Stage 2: Gradient computations;
+   for (k=0;k<*nObs;k++){
+     currentPair = -1;
+     for (i=0;i<(*nSite-1);i++){
+       for (j=i+1;j<*nSite;j++){
+	 
+	 currentPair++;
+	 
+	 c1 = sqrt(R_pow_di(data[k + i * *nObs], 2) + 
+		   R_pow_di(data[k + j * *nObs], 2) -
+		   2 * data[k + i * *nObs] * data[k + j * *nObs] *
+		   rho[currentPair]);
+
+	 B = (1 - R_pow_di(rho[currentPair], 2)) / 2 /
+	   R_pow_di(c1, 3);
+	 C = - (rho[currentPair] * data[k + i * *nObs] - c1 -
+		data[k + j * *nObs]) / 2 / c1 /
+	   R_pow_di(data[k + i * *nObs], 2);
+	 D = - (rho[currentPair] * data[k + j * *nObs] - c1 -
+		data[k + i * *nObs]) / 2 / c1 /
+	   R_pow_di(data[k + j * *nObs], 2);
+
+	 dA =  1 / 2 / c1;
+	 dB = - rho[currentPair] / R_pow_di(c1, 3) + 3 * 
+	   (1 - rho[currentPair]) * data[k + i * *nObs] *
+	   data[k + j * *nObs] / R_pow_di(c1, 5);
+	 dC = - (data[k + i * *nObs] - data[k + j * *nObs] *
+		 rho[currentPair]) / 2 / R_pow_di(c1, 3);
+	 dD = - (data[k + j * *nObs] - data[k + i * *nObs] *
+		 rho[currentPair]) / 2 / R_pow_di(c1, 3);
+		   
+
+	 jacCommon = dA + (dB * C + B * dC + dD) / (B*C + D);
+	 
+	 switch (*covmod){
+	 case 1:
+	   grad[k] = grad[k] - rho[currentPair] * *smooth / *scale -
+	     rho[currentPair] / R_pow_di(*scale, 2) + R_pow(2, 1 - *smooth) *
+	     R_pow(dist[currentPair] / *scale, *smooth) * dist[currentPair] * 
+	     bessel_k(dist[currentPair] / *scale, *smooth, 1) / 
+	     R_pow_di(*scale, 2) * jacCommon;
+	   
+	   //I found no analytical form for the partial derivative of
+	   //the Whittle-Matern covariance function with respect to
+	   //the smooth parameter. Thus, I use a finite difference
+	   //approach.
+	   grad[*nObs + k] = grad[*nObs + k] - M_LN2 * rho[currentPair] -
+	     rho[currentPair] * digamma(*smooth) + log(dist[currentPair] / *scale) *
+	     rho[currentPair] + R_pow(2, 1 - *smooth) * R_pow(dist[currentPair] / *scale, *smooth) *
+	     (bessel_k(dist[currentPair] / *scale, *smooth + 0.001, 1) - 
+	      bessel_k(dist[currentPair] / *scale, *smooth, 1)) / 0.001 / gammafn(*smooth) * jacCommon;
+	   break;
+	 case 2:
+	   grad[k] = grad[k] + rho[currentPair] * *smooth * 
+	     R_pow_di(dist[currentPair] / *scale, 2) * R_pow(rho[currentPair], 1 / *smooth) *
+	     jacCommon; 
+	   grad[*nObs + k] = grad[*nObs + k] - rho[currentPair] * 
+	     log(1 + R_pow_di(dist[currentPair], 2) / *scale) * jacCommon;
+	   break;
+	 case 3:
+	   grad[k] = grad[k] + R_pow(dist[currentPair] / *scale, *smooth) *
+	     *smooth * rho[currentPair] / *scale * jacCommon;
+	   grad[*nObs + k] = grad[*nObs + k] - R_pow(dist[currentPair] / *scale, *smooth) *
+	     log(dist[currentPair] / *scale) * rho[currentPair] * jacCommon;
+	   break;	   
+	 }
+       }
+     }
+   }
+   
+   return;
+}
       
   
