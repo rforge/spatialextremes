637a5854f702c7cd497a898180d5bea98989109b
ribatet <ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-05-07 15:53:31 +0000

    Passing src/header.h and man/internals.Rd with "ignore" prop. to svn
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SpatialExtremes/trunk/src/header.h
+++ SpatialExtremes/trunk/src/header.h
@@ -129,7 +129,12 @@ void smithgrad2(double *data, double *distVec, int *nSite,
 		int *nshapecoeff, double *loccoeff, double *scalecoeff,
 		double *shapecoeff, double *icov11, double *icov12,
 		double *icov22, int *fitmarge, double *grad);
-
+void schlathergrad(int *covmod, double *data, double *dist, int *nSite,
+		   int *nObs, double *locdsgnmat, int *nloccoeff,
+		   double *scaledsgnmat, int *nscalecoeff, double *shapedsgnmat,
+		   int *nshapecoeff, double *loccoeff, double *scalecoeff,
+		   double *shapecoeff, double *scale, double *smooth,
+		   int *fitmarge, double *grad);
 ///////////////////////////////////
 //  From pairwiselik.c
 //
