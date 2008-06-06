42ce98ca0c994f86f903082707c623277a666b32
ribatet <ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-06 08:13:07 +0000

    Modify the gev2frech and frech2gev functions to allow for a null shape parameter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SpatialExtremes/trunk/R/utils.R
+++ SpatialExtremes/trunk/R/utils.R
@@ -21,11 +21,21 @@ distance <- function(coord, vec = FALSE){
   return(dist)
 }
 
-gev2frech <- function(x, loc, scale, shape)
-  pmax(1 + shape * (x - loc) / scale, 0)^(1/shape)
+gev2frech <- function(x, loc, scale, shape){
+  if (shape == 0)
+    exp((x - loc)/scale)
+  
+  else
+    pmax(1 + shape * (x - loc) / scale, 0)^(1/shape)
+}
 
-frech2gev <- function(x, loc, scale, shape)
-  loc + scale * (pmax(x, 0)^shape - 1) / shape
+frech2gev <- function(x, loc, scale, shape){
+  if (shape == 0)
+    scale * log(pmax(x, 0)) + loc
+
+  else
+    loc + scale * (pmax(x, 0)^shape - 1) / shape
+}
 
 .qgev <- function(p, loc = 1, scale = 1, shape = 1,
                   lower.tail = TRUE){
