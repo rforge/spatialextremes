18a5da3c33e9e86027999ebbaa506ca72ffd5e3a
ribatet <ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-28 09:49:27 +0000

    Adding the ... argument to allow for hand fixed parameter in the GEV/GPD
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SpatialExtremes/trunk/R/univfit.R
+++ SpatialExtremes/trunk/R/univfit.R
@@ -1,32 +1,53 @@
-gevmle <- function(x, method = "BFGS"){
+gevmle <- function(x, ..., method = "BFGS"){
 
   n <- length(x)
+  param <- c("loc", "scale", "shape")
   
-  nlgev <- function(param)
-    ##param = c(loc, scale, shape)
+  nlgev <- function(loc, scale, shape)
     -.C("gevlik", as.double(x), as.integer(n),
-        as.double(param[1]), as.double(param[2]),
-        as.double(param[3]), dns = double(1),
+        as.double(loc), as.double(scale),
+        as.double(shape), dns = double(1),
         PACKAGE = "SpatialExtremes")$dns
   
   start <- c(loc = 0, scale = 0, shape = 0)
   start["scale"] <- sqrt(6 * var(x)) / pi
   start["loc"] <- mean(x) - 0.58 * start["scale"]
+
+  start <- start[!(param %in% names(list(...)))]
+
+  nm <- names(start)
+  l <- length(nm)
+  f <- formals(nlgev)
+  names(f) <- param
+  m <- match(nm, param)
+
+  formals(nlgev) <- c(f[m], f[-m])
+  nllh <- function(p, ...) nlgev(p, ...)
   
-  opt <- optim(start, nlgev, method = method)
-  param <- opt$par
+  if(l > 1)
+    body(nllh) <- parse(text = paste("nlgev(", paste("p[",1:l,
+                          "]", collapse = ", "), ", ...)"))
+  
+  fixed.param <- list(...)[names(list(...)) %in% param]
+  
+  if(any(!(param %in% c(nm,names(fixed.param)))))
+    stop("unspecified parameters")
+    
+  opt <- optim(start, nllh, ..., method = method)
+  param <- c(opt$par, unlist(fixed.param))
   names(param) <- c("loc", "scale", "shape")
 
   return(param)
 }
 
 
-gpdmle <- function(x, threshold, method = "BFGS"){
+gpdmle <- function(x, threshold, ..., method = "BFGS"){
 
-  nlgpd <- function(param){
-    ##param = c(scale, shape)
+  param <- c("scale", "shape")
+  
+  nlgpd <- function(scale, shape){
     -.C("gpdlik", as.double(exceed), as.integer(nat), as.double(threshold),
-        as.double(param[1]), as.double(param[2]), dns = double(1),
+        as.double(scale), as.double(shape), dns = double(1),
         PACKAGE = "SpatialExtremes")$dns
   }
 
@@ -34,10 +55,30 @@ gpdmle <- function(x, threshold, method = "BFGS"){
   exceed <- as.double(x[high])
   nat <- as.integer(exceed)
 
-  start <- c(mean(exceed) - min(threshold), 0.0)
+  start <- c(scale = mean(exceed) - min(threshold), shape = 0.0)
+
+  start <- start[!(param %in% names(list(...)))]
+
+  nm <- names(start)
+  l <- length(nm)
+  f <- formals(nlgpd)
+  names(f) <- param
+  m <- match(nm, param)
+
+  formals(nlgpd) <- c(f[m], f[-m])
+  nllh <- function(p, ...) nlgpd(p, ...)
   
-  opt <- optim(start, nlgpd, method = method)
-  param <- opt$par
+  if(l > 1)
+    body(nllh) <- parse(text = paste("nlgpd(", paste("p[",1:l,
+                          "]", collapse = ", "), ", ...)"))
+  
+  fixed.param <- list(...)[names(list(...)) %in% param]
+  
+  if(any(!(param %in% c(nm,names(fixed.param)))))
+    stop("unspecified parameters")
+  
+  opt <- optim(start, nllh, ..., method = method)
+  param <- c(opt$par, unlist(fixed.param))
   names(param) <- c("scale", "shape")
 
   return(param)
