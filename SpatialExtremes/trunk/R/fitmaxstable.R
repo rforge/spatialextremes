7ef30d7abc265da541e6fa181a6d6cfcfda01d60
ribatet <ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-30 07:04:53 +0000

    Moving the fit.marge argument
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@156 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SpatialExtremes/trunk/R/fitmaxstable.R
+++ SpatialExtremes/trunk/R/fitmaxstable.R
@@ -1,6 +1,6 @@
 fitmaxstab <- function(data, coord, cov.mod = c("gauss", "whitmat", "cauchy", "powexp"),
-                       loc.form, scale.form, shape.form, fit.marge = TRUE,
-                       marg.cov = NULL, ..., warn.inf = TRUE, method = "BFGS",
+                       loc.form, scale.form, shape.form, marg.cov = NULL, ...,
+                       fit.marge = TRUE, warn.inf = TRUE, method = "BFGS",
                        control = list(), std.err.type = "score", corr = FALSE){
 
   if (nrow(coord) != ncol(data))
@@ -17,6 +17,7 @@ fitmaxstab <- function(data, coord, cov.mod = c("gauss", "whitmat", "cauchy", "p
   
   if (!missing(loc.form) && !missing(scale.form) && !missing(shape.form)){
     reg.mod <- "spatgev"
+    fit.marge <- TRUE
     
     if ((class(loc.form) != "formula") || (class(scale.form) != "formula") ||
         (class(shape.form) != "formula"))
@@ -28,7 +29,7 @@ fitmaxstab <- function(data, coord, cov.mod = c("gauss", "whitmat", "cauchy", "p
   if (!(flag %in% c(0, 3)))
     stop("if one formula is given for the GEV parameters, then it should
 be given for *ALL* GEV parameters")
-  
+
   if (cov.mod == "gauss")
     fitted <- switch(reg.mod, "full" = smithfull(data, coord, ..., fit.marge = fit.marge,
                                 warn.inf = warn.inf, method = method, control = control,
