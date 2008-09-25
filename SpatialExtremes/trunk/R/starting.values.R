a78a41c6042ee7374536787076be522e76ad2d68
ribatet <ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-09-25 13:38:06 +0000

    Starting values are computed more robustly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@208 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SpatialExtremes/trunk/R/starting.values.R
+++ SpatialExtremes/trunk/R/starting.values.R
@@ -26,13 +26,16 @@
   }
 
   if (length(fixed.param) > 0){
-    args <- c(list(data = dataFrech, coord = coord, method = "Nelder", marge = "frech"),
+    args <- c(list(data = data, coord = coord, marge = "emp"),
               fixed.param)
     covs <- do.call("fitcovmat", args)$param
   }
 
   else
-    covs <- fitcovmat(dataFrech, coord, method = "Nelder", marge = "frech")$param  
+    covs <- fitcovmat(data, coord, marge = "emp")$param  
+
+  covs <- smithfull(dataFrech, coord, start = as.list(covs),
+                    fit.marge = FALSE, method = method, warn = FALSE)$param
   
   locCoeff <- loc.model$init.fun(loc)
   scaleCoeff <- scale.model$init.fun(scale)
@@ -98,22 +101,18 @@
   scaleCoeff[is.na(scaleCoeff)] <- 0
   shapeCoeff[is.na(shapeCoeff)] <- 0
   
-  ##To be sure that the scale parameter is always positive at starting
-  ##values
-  scales.hat <- scale.model$dsgn.mat %*% scaleCoeff
-
-  if (any(scales.hat <= 0))
-    scaleCoeff[1] <- scaleCoeff[1] - min(scales.hat) + .1
-
   if (length(fixed.param) > 0){
-    args <- c(list(data = dataFrech, coord = coord, cov.mod = cov.mod,
-                   method = "Nelder", marge = "frech"), fixed.param)
+    args <- c(list(data = data, coord = coord, cov.mod = cov.mod,
+                   marge = "emp"), fixed.param)
     cov.param <- do.call("fitcovariance", args)$param
   }
   
   else
-    cov.param <- fitcovariance(dataFrech, coord, cov.mod, method = "Nelder",
-                               marge = "frech")$param
+    cov.param <- fitcovariance(data, coord, cov.mod, marge = "emp")$param
+
+  cov.param <- schlatherfull(dataFrech, coord, start = as.list(cov.param),
+                             cov.mod = cov.mod, fit.marge = FALSE, method = method,
+                             warn = FALSE)$param
   
   start <- as.list(cov.param)
 
