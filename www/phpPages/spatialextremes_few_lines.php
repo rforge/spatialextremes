c979a85afed5dd67ee5c3ab63d79ad6358e2ee78
mathieu_ribatet <mathieu_ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-07 11:23:47 +0000

    Homogeneize the layout
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@241 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/phpPages/spatialextremes_few_lines.php
+++ www/phpPages/spatialextremes_few_lines.php
@@ -3,7 +3,7 @@ the package. However, for a full description, users may want to
 have a look to the package vignette and the html help of the
 package.
 
-<h4 id="GPDComp">Fit a max-stable process - assuming unit Frechet
+<h4 id="maxstabfrech">Fit a max-stable process - assuming unit Frechet
   margins:</h4>
 <div class="Rcodes">
   <span class="Rcomments"> 
@@ -27,7 +27,7 @@ package.
     </span>
   </a>
 </div>
-<h4 id="GPDFit">Fit a max-stable process - allowing for common GEV
+<h4 id="maxstabGEV">Fit a max-stable process - allowing for common GEV
   margins</h4>
 <div class="Rcodes">
   <span class="Rcomments">
@@ -48,12 +48,12 @@ package.
       y ~ lat, shape.form = y ~ 1)</code>
   </a>
 </div>  
-<h4 id="GPDPlot">Plots</h4>
+<h4 id="plots">Plots</h4>
 <div class="Rcodes">
   <span class="Rcomments">
     ##Covariance functions:<br/></span>
   <a href="images/covariance.png" class="Routs">
-    <code class="Routs">covariance(sill = 1, range = 1, smooth = 0.5, cov.mod = "whitmat");</code>
+    <code>covariance(sill = 1, range = 1, smooth = 0.5, cov.mod = "whitmat");</code>
     <span>
       <img src="images/covariance.png"
 	   alt="covariance function"
@@ -64,7 +64,7 @@ package.
   <span class="Rcomments">
     ##Extremal coefficient:<br/> </span>
   <a class="Routs" href="images/extcoeff.png">
-    <code class="Routs">extcoeff(fitted.object);</code>
+    <code>extcoeff(fitted.object);</code>
     <span>
       <img src="images/extcoeff.png" 
 	   alt="extremal coefficient"
@@ -76,22 +76,22 @@ package.
     ##Prediction of return levels:<br/> 
   </span>
   <a class="Routs" href="images/map.png">
-    <code class="Routs">map(fitted.object, "quant", ret.per =
+    <code>map(fitted.object, "quant", ret.per =
     50)</code>
     <span>
       <img src="images/map.png"
 	   alt="prediction of return levels"
-	   height="150"/>
+	   height="300"/>
     </span>
   </a>
   <br/>
   <a class="Routs" href="images/condmap.png">
-    <code class="Routs">condmap(schlather, c(1, 1), seq(0, 10, length
+    <code>condmap(schlather, c(1, 1), seq(0, 10, length
     = 20), seq(0,10, length = 20))</code>
     <span>
       <img src="images/condmap.png"
 	   alt="prediction of conditional return levels"
-	   height="150"/>
+	   height="300"/>
     </span>
   </a>
   <br/>
@@ -99,8 +99,8 @@ package.
     ##Semi-parametric estimates of the extremal coefficient:<br/> 
   </span>
   <a class="Routs" href="images/fitextcoeff.png">
-    <code class="Routs">fitextcoeff(data, coord); fitextcoeff(data,
-    coord, angles = seq(-pi, pi, length = 4))</code>
+    <code>fitextcoeff(data, coord); fitextcoeff(data, coord, angles =
+    seq(-pi, pi, length = 4))</code>
     <span>
       <img src="images/fitextcoeff.png"
 	   alt="semi-parametric estimates of the extremal coefficient"
@@ -112,8 +112,7 @@ package.
     ##Profile composite likelihood:<br/> 
   </span>
   <a class="Routs" href="images/proflik.png">
-    <code class="Routs">profile(fitted.object, "cov11", range = c(80,
-    200))</code>
+    <code>profile(fitted.object, "cov11", range = c(80, 200))</code>
     <span>
       <img src="images/proflik.png"
 	   alt="Profile composite likelihood"
