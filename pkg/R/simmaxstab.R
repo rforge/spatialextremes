439f2dd1fadd14b58a693499a9fcaefe23145860
mathieu_ribatet <mathieu_ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-05 21:39:13 +0000

    Extend the simulation from the Smith model
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@410 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/simmaxstab.R
+++ pkg/R/simmaxstab.R
@@ -13,26 +13,19 @@ rmaxstab <- function(n, coord, model = "gauss", cov11 = 1, cov12 = 0,
   coord.range <- apply(coord, 2, range)
   center <- colMeans(coord.range)
   edge <- max(apply(coord.range, 2, diff))
-   
-  if (grid){
-    new.coord <- NULL
-    n.grid <- nrow(coord)
-    n.site <- n.grid^2
-    n <- 1
-
-    for (x in coord[,1])
-      new.coord <- rbind(new.coord, cbind(x, coord[,2]))
-
-    coord <- new.coord
-    n.site <- nrow(coord)
-  }
-
-  ans <- .C("rsmith", as.double(coord), as.double(center), as.double(edge),
-            as.integer(n), as.integer(n.site), as.double(cov11), as.double(cov12),
-            as.double(cov22), ans = double(n.site * n), PACKAGE = "SpatialExtremes")$ans
 
   if (grid)
-    ans <- matrix(ans, n.grid, byrow = TRUE)
+    ans <- double(n.site * n.site * n)
+
+  else
+    ans <- double(n.site * n)
+  
+  ans <- .C("rsmith", as.double(coord), as.double(center), as.double(edge),
+            as.integer(n), as.integer(n.site), grid, as.double(cov11), as.double(cov12),
+            as.double(cov22), ans = ans, PACKAGE = "SpatialExtremes")$ans
+
+  if (grid)
+    ans <- array(ans, c(n.site, n.site, n))
 
   else
     ans <- matrix(ans, nrow = n, ncol = n.site, byrow = TRUE)
--- pkg/src/simmaxstab.c
+++ pkg/src/simmaxstab.c
@@ -1,12 +1,13 @@
 #include "header.h"
 
 void rsmith(double *coord, double *center, double *edge, int *nObs,
-	    int *nSites, double *cov11, double *cov12, double *cov22,
-	    double *ans){
+	    int *nSites, int *grid, double *cov11, double *cov12,
+	    double *cov22, double *ans){
   /* coord: the coordinates of the locations
     center: the center of the compact set - here I use a square
       edge: the length of the edge of the square
       nObs: the number of observations to be generated
+      grid: Does coord specifies a grid?
     nSites: the number of locations
      covXX: the parameters of the bivariate normal density
        ans: the generated random field */
@@ -30,40 +31,85 @@ void rsmith(double *coord, double *center, double *edge, int *nObs,
      set need to be inflated first */
   *edge += 3.46 * sqrt(fmax2(*cov11, *cov22));
   lebesgue = *edge * *edge;
-  
+
   GetRNGstate();
-  for (i=*nObs;i--;){
-    inSites = i * *nSites;
-    poisson = 0;
-    nKO = *nSites;
+  
+  if (*grid){
+    //Simulation part if a grid is used
+    for (i=*nObs;i--;){
+      inSites = i * *nSites * *nSites;
+      poisson = 0;
+      nKO = *nSites * *nSites;
     
-    while (nKO) {
-      /* The stopping rule is reached when nKO = 0 i.e. when each site
-	 satisfies the condition in Eq. (8) of Schlather (2002) */
+      while (nKO) {
+	/* The stopping rule is reached when nKO = 0 i.e. when each site
+	   satisfies the condition in Eq. (8) of Schlather (2002) */
 
-      poisson += exp_rand();
-      ipoisson = 1 / poisson;
-      thresh = uBound * ipoisson;
+	poisson += exp_rand();
+	ipoisson = 1 / poisson;
+	thresh = uBound * ipoisson;
 
-      //We simulate points uniformly in [-r/2, r/2]^2
-      u1 = *edge * runif(-0.5, 0.5);
-      u2 = *edge * runif(-0.5, 0.5);
+	//We simulate points uniformly in [-r/2, r/2]^2
+	u1 = *edge * runif(-0.5, 0.5);
+	u2 = *edge * runif(-0.5, 0.5);
       
-      nKO = *nSites;
-      for (j=*nSites;j--;){
-	if (thresh > ans[j + inSites]){
-	  /* This is the bivariate normal density with 0 mean and
-	     cov. matrix [cov11, cov12; cov12, cov22] */
-	  y = exp((-*cov22 * (coord[j] - u1) * (coord[j] - u1) + 2 * *cov12 *
-		   (coord[j] - u1) * (coord[*nSites + j] - u2) - *cov11 *
-		   (coord[*nSites + j] - u2) * (coord[*nSites + j] - u2)) *
-		  itwiceDet) * thresh;
-	  
-	  ans[j + inSites] = fmax2(y, ans[j + inSites]);
-	}
+	nKO = *nSites * *nSites;
+	for (j=*nSites;j--;){
+	  for (k=*nSites;k--;){
+	    if (thresh > ans[j + k * *nSites + inSites]){
+	      /* This is the bivariate normal density with 0 mean and
+		 cov. matrix [cov11, cov12; cov12, cov22] */
+	      y = exp((-*cov22 * (coord[j] - u1) * (coord[j] - u1) + 2 * *cov12 *
+		       (coord[j] - u1) * (coord[*nSites + k] - u2) - *cov11 *
+		       (coord[*nSites + k] - u2) * (coord[*nSites + k] - u2)) *
+		      itwiceDet) * thresh;
+	      
+	      ans[j + k * * nSites + inSites] = fmax2(y, ans[j + k * *nSites + inSites]);
+	    }
 	
-	else
-	  nKO--;
+	    else
+	      nKO--;
+	  }
+	}
+      }
+    }
+  }
+
+  else{
+    //Simulation part if a grid isn't used
+    for (i=*nObs;i--;){
+      inSites = i * *nSites;
+      poisson = 0;
+      nKO = *nSites;
+    
+      while (nKO) {
+	/* The stopping rule is reached when nKO = 0 i.e. when each site
+	   satisfies the condition in Eq. (8) of Schlather (2002) */
+
+	poisson += exp_rand();
+	ipoisson = 1 / poisson;
+	thresh = uBound * ipoisson;
+
+	//We simulate points uniformly in [-r/2, r/2]^2
+	u1 = *edge * runif(-0.5, 0.5);
+	u2 = *edge * runif(-0.5, 0.5);
+      
+	nKO = *nSites;
+	for (j=*nSites;j--;){
+	  if (thresh > ans[j + inSites]){
+	    /* This is the bivariate normal density with 0 mean and
+	       cov. matrix [cov11, cov12; cov12, cov22] */
+	    y = exp((-*cov22 * (coord[j] - u1) * (coord[j] - u1) + 2 * *cov12 *
+		     (coord[j] - u1) * (coord[*nSites + j] - u2) - *cov11 *
+		     (coord[*nSites + j] - u2) * (coord[*nSites + j] - u2)) *
+		    itwiceDet) * thresh;
+	  
+	    ans[j + inSites] = fmax2(y, ans[j + inSites]);
+	  }
+	
+	  else
+	    nKO--;
+	}
       }
     }
   }
@@ -72,8 +118,15 @@ void rsmith(double *coord, double *center, double *edge, int *nObs,
 
   /* Lastly, we multiply by the Lebesgue measure of the dilated
     compact set */
-  for (i=(*nSites * *nObs);i--;)
-    ans[i] *= lebesgue;
+  if (*grid){
+    for (i=(*nSites * *nSites * *nObs);i--;)
+      ans[i] *= lebesgue;
+  }
+  
+  else{
+    for (i=(*nSites * *nObs);i--;)
+      ans[i] *= lebesgue;
+  }
 
   return;
 }
