cb715259b3d93a1530f8d68e83137026bca90f1b
mathieu_ribatet <mathieu_ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-09 08:01:52 +0000

    Some old attempt to get a like a book reader were still here
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@652 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/modules/pages/manual.php
+++ www/modules/pages/manual.php
@@ -3,11 +3,4 @@
   The reference manual, i.e., a document describing all the functions
   implemented in the package, is available
   from <a href="modules/docs/SpatialExtremes-manual.pdf">here</a>.
-
-
-  <div id="turner">
-    <div id="flip"></div>
-    <div id="leftpage"></div>
-    <div id="rightpage"></div>
-  </div>
 </div>
