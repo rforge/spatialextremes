c0d5bbe9a5492c271648d9c163f7144f8feac9b5
mathieu_ribatet <mathieu_ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-06 18:43:42 +0000

    Fix typos
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@670 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/global/haut.php
+++ www/global/haut.php
@@ -17,7 +17,7 @@
     to provide tools  to model spatial extremes." />
     <meta name="Author" content="Mathieu Ribatet" />
     <link rel="icon" type="image/png" href="images/favicon.png" />
-    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
     <link href="/styles.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="imageflow.css" type="text/css" />
     <link rel="stylesheet" href="highslide.css" type="text/css" />
