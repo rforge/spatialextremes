beb95ce026fae9991f605fadc60ee0b64b747b84
mathieu_ribatet <mathieu_ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-18 07:48:55 +0000

    Add a link to the package vignette from the web page.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@695 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/modules/pages/manual.php
+++ www/modules/pages/manual.php
@@ -3,5 +3,10 @@
 <div id="right">
   The reference manual, i.e., a document describing all the functions
   implemented in the package, is available
-  from <a href="modules/docs/SpatialExtremes-manual.pdf">here</a>.
+  from <a href="modules/docs/SpatialExtremes-manual.pdf">here</a>. Some
+  years ago, I started writing a package vignette introducing the
+  theory of max-stable processes and how to use the package in
+  concrete situations. Unfortunately the vignette is no longer updated
+  and many new feature are not described. You can found the vignette
+  <a href="modules/docs/SpatialExtremesGuide.pdf">here</a>.
 </div>
