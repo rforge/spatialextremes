b4fa6b68b6dcfca2c933e4c489dd75f907054eb5
mathieu_ribatet <mathieu_ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-04 13:32:43 +0000

    Some minor improvements
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@739 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/gradientPllik.R
+++ pkg/tests/gradientPllik.R
@@ -9,13 +9,13 @@ coord <- matrix(runif(2 * n.site, 0, 10), n.site)
 data <- rmaxstab(n.obs, coord, "gauss", cov11 = 3, cov12 = 0, cov22 = 3)
 
 M1 <- fitmaxstab(log(data), coord, "gauss", y ~ 1, y ~ 1, y ~ 1,
-                 control = list(check.grad = TRUE), iterlim = 2)
+                 check.grad = TRUE, iterlim = 2)
 
 M2 <- fitmaxstab(log(data), coord, "whitmat", y ~ 1, y ~ 1, y ~ 1,
-                 control = list(check.grad = TRUE), iterlim = 2)
+                 check.grad = TRUE, iterlim = 2)
 
 M3 <- fitmaxstab(log(data), coord, "gpowexp", y ~ 1, y ~ 1, y ~ 1,
-                 control = list(check.grad = TRUE), iterlim = 2)
+                 check.grad = TRUE, iterlim = 2)
 
 M4 <- fitmaxstab(log(data), coord, "brown", y ~ 1, y ~ 1, y ~ 1,
-                 control = list(check.grad = TRUE), iterlim = 2)
+                 check.grad = TRUE, iterlim = 2)
