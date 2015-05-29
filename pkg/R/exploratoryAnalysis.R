35efe16f99a59bb4176e29298550ca212aae66ee
mathieu_ribatet <mathieu_ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-29 08:35:30 +0000

    Summary: Try to improve symbolplot to allow for a legend. Not yet done!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@804 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/exploratoryAnalysis.R
+++ pkg/R/exploratoryAnalysis.R
@@ -1,15 +1,20 @@
-symbolplot <- function(data, coord, which = "gev", plot.border = NULL, col = c("#FF000080", "#0000FF80")){
+symbolplot <- function(data, coord, which = "gev", plot.border = NULL, col = c("#FF000080", "#0000FF80"),
+                       plot.legend = TRUE){
 
     if (!(which %in% c("gev", "mean", "median")))
         stop("'which' must be one of 'gev', 'mean' or 'median'")
-
 
     add <- FALSE
     if (which == "gev"){
         values <- apply(data, 2, gevmle)
         ref.value <- rowMeans(values)
-        
-        par(mfrow = c(1, 3))
+
+        if (plot.legend)
+            layout(matrix(c(2,1,4,3,6,5), 2), height = c(0.15, 1))
+        else
+            par(mfrow = c(1, 3))
+
+        mar <- par("mar")
 
         for (i in 1:3){
             if (!is.null(plot.border)){
@@ -20,18 +25,42 @@ symbolplot <- function(data, coord, which = "gev", plot.border = NULL, col = c("
             sign.col <- col[2 - (values[i,] >= ref.value[i])]
             radius <- abs(values[i,] - ref.value[i])
             norm.factor <-  min(dist(coord)) / max(radius)
-            radius <- norm.factor * radius
-            symbols(coord, circles = radius, add = add, bg = sign.col, inches = FALSE)
+            symbols(coord, circles = radius * norm.factor, add = add, bg = sign.col, inches = FALSE)
+
+            if (plot.legend){
+                radius.legend <- pretty(radius, 4)
+                radius.legend <- radius.legend[radius.legend > 0]
+                n.circles <- length(radius.legend)
+
+                xlim <- c(0, diff(par("usr")[1:2]))
+                centers <- cbind(seq(xlim[1], xlim[2], length = n.circles), 0)
+
+                par(mar = c(0, 5, 0, 5))
+                symbols(centers, circles = radius.legend * norm.factor, inches = FALSE, bty = "n",
+                        xaxt = "n", yaxt = "n", xlab = "", ylab = "")
+
+                for (j in 1:n.circles)
+                    text(centers[j,1], -max(radius.legend) * norm.factor,
+                         bquote(phantom(a)%+-%.(radius.legend[j])), pos = 1, offset = 0.75)
+                
+                par(mar = mar)
+            }
         }
     }
 
     else {
+                
         if (which == "mean")
             values <- colMeans(data, na.rm = TRUE)
         else
             values <- apply(data, 2, median, na.rm = TRUE)
 
         ref.value <- mean(values, na.rm = TRUE)
+
+        if (plot.legend)
+            layout(matrix(2:1, 2), height = c(0.15, 1))
+        
+        mar <- par("mar")
 
         if (!is.null(plot.border)){
             plot.border()
@@ -41,9 +70,30 @@ symbolplot <- function(data, coord, which = "gev", plot.border = NULL, col = c("
         sign.col <- col[2 - (values >= ref.value)]
         radius <- abs(values - ref.value)
         norm.factor <- min(dist(coord)) / max(radius)
-        radius <- norm.factor * radius
 
-        symbols(coord, circles = radius, add = add, bg = sign.col, inches = FALSE)
+        symbols(coord, circles = radius * norm.factor, add = add, bg = sign.col, inches = FALSE)
+
+        for (j in 1:length(radius))
+            text(coord[j,1], coord[j,2], bquote(.(round(radius[j], 1))))
+
+        if (plot.legend){
+            radius.legend <- pretty(radius, 4)
+            radius.legend <- radius.legend[radius.legend > 0]
+            n.circles <- length(radius.legend)
+
+            xlim <- c(0, diff(par("usr")[1:2]))
+            centers <- cbind(seq(xlim[1], xlim[2], length = n.circles), 0)
+
+            par(mar = c(0, 5, 0, 5))
+            symbols(centers, circles = radius.legend * norm.factor, inches = FALSE, bty = "n",
+                    xaxt = "n", yaxt = "n", xlab = "", ylab = "")
+
+            for (j in 1:n.circles)
+                text(centers[j,1], -max(radius.legend) * norm.factor,
+                     bquote(phantom(a)%+-%.(radius.legend[j])), pos = 1, offset = 0.75)
+                
+                par(mar = mar)
+            }
     }
 
     par(new = FALSE)
