af688ce670f2af07adf19a12865d4e4ce253f639
mathieu_ribatet <mathieu_ribatet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-02-14 06:47:07 +0000

    Update the news file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatialextremes@890 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/modules/pages/news.php
+++ www/modules/pages/news.php
@@ -6,10 +6,14 @@
       <div class="munth_box">
         <span>Exact simulations</span> <br />
         Exact simulations of max-stable processes are now available using the fantactic job
-	of <a href="https://academic.oup.com/biomet/article/103/2/303/1744000">C. Dombry, S. Engelke and M. Oesting</a>.
-        <div class="read_l"><a href="index.php?module=pages&amp;action=maxLinear">read
+	of C. Dombry, S. Engelke and M. Oesting.
+        <div class="read_l"><a href="index.php?module=pages&amp;action=maxstable">read
         more</a></div>
-      </div>      
+      </div>
+      <div class="munth_box">
+        <span>Parallelization</span> <br />
+        The package makes uses of OpenMP if available on your platform. You should have substantial CPU time gains. Well I hope...
+      </div>    
     </div>
     <div class="box_bot"></div>
   </div>
